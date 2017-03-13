b63c367cb923c6c54ddecfe7624fd8db87bd1538
rain <racoonwr@gmail.com>
2017-03-13 21:47:05 +0800

    modify server listen port

--- mcw_demo_service/src/index.js
+++ mcw_demo_service/src/index.js
@@ -5,6 +5,6 @@ serverIns.get('user/all', function respond(req, res, next){
     select_from_g_user(req, res, next)
 });
 
-serverIns.listen(8080, function () {
+serverIns.listen(8081, function () {
     console.log('%s listening at %s', serverIns.name, serverIns.url);
 });
